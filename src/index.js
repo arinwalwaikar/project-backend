525af94c125ddffd0f1eb655df8434fcecbde793
Arin Walwaikar <arinwalwaikar@gmail.com>
2026-04-30 17:53:12 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,37 @@
+
+//require('dotenv').config({path: '.env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+     path: './env' 
+});
+
+
+
+connectDB();
+
+/*
+
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+import express from "express";
+const app = express();
+
+;(async () => {
+    try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
+
+    app.listen(3000, () => {
+        console.log(`Server is running on ${process.env.PORT}`);
+    });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+})()
+*/
